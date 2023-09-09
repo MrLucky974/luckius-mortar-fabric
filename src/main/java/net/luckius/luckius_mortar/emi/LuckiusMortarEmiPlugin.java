65f2df200995330e13ec361c2f2397174643bff9
MrLucky974 <HeroCreepyFR@gmail.com>
2023-09-09 10:34:37 +0400

    Starting EMI implementation

---
+++ src/main/java/net/luckius/luckius_mortar/emi/LuckiusMortarEmiPlugin.java
@@ -0,0 +1,22 @@
+package net.luckius.luckius_mortar.emi;
+
+import dev.emi.emi.api.EmiPlugin;
+import dev.emi.emi.api.EmiRegistry;
+import dev.emi.emi.api.recipe.EmiRecipeCategory;
+import dev.emi.emi.api.render.EmiTexture;
+import dev.emi.emi.api.stack.EmiStack;
+import net.luckius.luckius_mortar.LuckiusMortar;
+import net.minecraft.util.Identifier;
+
+public class LuckiusMortarEmiPlugin implements EmiPlugin {
+    /*public static final Identifier MY_SPRITE_SHEET = new Identifier(LuckiusMortar.MOD_ID, "textures/gui/emi_simplified_textures.png");
+    public static final EmiStack MY_WORKSTATION = EmiStack.of(MyModItems.MY_WORKSTATION);
+    public static final EmiRecipeCategory MY_CATEGORY
+            = new EmiRecipeCategory(MY_WORKSTATION, new EmiTexture(MY_SPRITE_SHEET, 0, 0, 16, 16));
+    */
+
+    @Override
+    public void register(EmiRegistry registry) {
+
+    }
+}
